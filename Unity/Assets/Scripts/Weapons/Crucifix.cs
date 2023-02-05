0b7e8c8aaa1247c5e112ff0ef8eb50c1916600d2
juananre <juanandresferrerduenas@gmail.com>
2023-02-04 23:21:52 -0500

    perdon, culpa de gabriel(mi hermano))

--- Unity/Assets/Scripts/Weapons/Crucifix.cs
+++ Unity/Assets/Scripts/Weapons/Crucifix.cs
@@ -4,5 +4,34 @@ using UnityEngine;
 
 public class Crucifix : BaseWeapon
 {
+    [SerializeField] GameObject CrucifixProyectilePrefab;
+    public float lifeTime = 1f;
 
+    protected override void Awake()
+    {
+        base.Awake();
+        StartCoroutine(UseWeapon());
+    }
+
+    IEnumerator UseWeapon()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(cdTime);
+            target = aim.GetClosestEnemy();
+
+            if (target != null)
+            {
+                GameObject newProyectile = Instantiate(CrucifixProyectilePrefab, transform.position, Quaternion.identity, aim.proyectileHierarchyContainer);
+                GameObject newProyectile2 = Instantiate(CrucifixProyectilePrefab, transform.position, Quaternion.identity, aim.proyectileHierarchyContainer);
+                GameObject newProyectile3 = Instantiate(CrucifixProyectilePrefab, transform.position, Quaternion.identity, aim.proyectileHierarchyContainer);
+
+                newProyectile.GetComponent<StraightProyectile>().Initialize(aim.GetAimDirection(transform, target), lifeTime);
+                newProyectile2.GetComponent<StraightProyectile>().Initialize(aim.GetAimDirection(transform, target), lifeTime);
+                newProyectile3.GetComponent<StraightProyectile>().Initialize(aim.GetAimDirection(transform, target), lifeTime);
+            }
+
+            target = null;
+        }
+    }
 }
--- Unity/Assets/Scripts/Weapons/Halfmoon.cs
+++ Unity/Assets/Scripts/Weapons/Halfmoon.cs
@@ -4,5 +4,35 @@ using UnityEngine;
 
 public class Halfmoon : BaseWeapon
 {
+    [SerializeField] GameObject HalfmoonProyectilePrefab;
+    public float lifeTime = 1f;
 
+    protected override void Awake()
+    {
+        base.Awake();
+        StartCoroutine(UseWeapon());
+    }
+
+    IEnumerator UseWeapon()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(cdTime);
+            target = aim.GetClosestEnemy();
+
+            if (target != null)
+            {
+                GameObject newProyectile = Instantiate(HalfmoonProyectilePrefab, transform.position, Quaternion.identity, aim.proyectileHierarchyContainer);
+                GameObject newProyectile2 = Instantiate(HalfmoonProyectilePrefab, transform.position, Quaternion.identity, aim.proyectileHierarchyContainer);
+                GameObject newProyectile2 = Instantiate(HalfmoonProyectilePrefab, transform.position, Quaternion.identity, aim.proyectileHierarchyContainer);
+                GameObject newProyectile3 = Instantiate(HalfmoonProyectilePrefab, transform.position, Quaternion.identity, aim.proyectileHierarchyContainer);
+                
+                newProyectile.GetComponent<StraightProyectile>().Initialize(aim.GetAimDirection(transform, target), lifeTime);
+                newProyectile2.GetComponent<StraightProyectile>().Initialize(aim.GetAimDirection(transform, target), lifeTime);
+                newProyectile3.GetComponent<StraightProyectile>().Initialize(aim.GetAimDirection(transform, target), lifeTime);
+            }
+
+            target = null;
+        }
+    }
 }
