1b38ab9430c2c7361f0a9bcb151153a94bc1c725
Juan Fernando López Rodríguez <gotensferxl@gmail.com>
2023-02-04 23:33:18 -0500

    Sistema de eras con fases + metodos pa UI

--- Unity/Assets/Scripts/Managers/AgeManager.cs
+++ Unity/Assets/Scripts/Managers/AgeManager.cs
@@ -9,26 +9,69 @@ public enum Age
     Ancient = 1,
     Medieval = 2,
     Modern = 3,
-    Contemporary = 4
+}
+
+public enum GamePath
+{
+    Main = 0,
+    Secondary = 1
 }
 
 public class AgeManager : MonoBehaviour
 {
     public static Age age = Age.Primitive;
+    public static GamePath path = GamePath.Main;    
 
-    [SerializeField] GameObject[] weaponsAtAge;
     [SerializeField] Transform player;
+    [SerializeField] GameObject primitiveWeapon, ancientMain, ancientSecondary, medievalMain, medievalSecondary, 
+        modernMain, modernSecondary;
 
     public static UnityEvent onAgeChange = new();
 
     private void Start()
     {
         age = Age.Primitive;
-        onAgeChange.AddListener(SetAgeWeapon);
+    }
+
+    public void ChooseMainPath()
+    {
+        path = GamePath.Main;
+    }
+
+    public void ChooseSecondaryPath()
+    {
+        path = GamePath.Secondary;
+    }
+
+    public void ProvideWeapon()
+    {
+        SetAgeWeapon();
     }
 
     void SetAgeWeapon()
     {
-        Instantiate(weaponsAtAge[(int)age], Vector3.zero, Quaternion.identity, player);
+        switch (age)
+        {
+            case Age.Primitive:
+                AddWeaponToPlayer(primitiveWeapon);
+                break;
+            case Age.Ancient:
+                if (path == GamePath.Main) AddWeaponToPlayer(ancientMain);
+                else AddWeaponToPlayer(ancientSecondary);
+                break;
+            case Age.Medieval:
+                if (path == GamePath.Main) AddWeaponToPlayer(medievalMain);
+                else AddWeaponToPlayer(medievalSecondary);
+                break;
+            case Age.Modern:
+                if (path == GamePath.Main) AddWeaponToPlayer(modernMain);
+                else AddWeaponToPlayer(modernSecondary);
+                break;
+        }
+    }
+
+    void AddWeaponToPlayer(GameObject weapon)
+    {
+        Instantiate(weapon, Vector3.zero, Quaternion.identity, player);
     }
 }
