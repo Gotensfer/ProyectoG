7cae686fc64f6d40ec362e9cff676d689f2ca38b
jaydeeay <jarroyavep@outlook.com>
2023-02-05 00:38:45 -0500

    Update UI

--- Unity/Assets/Scripts/Managers/UIManager.cs
+++ Unity/Assets/Scripts/Managers/UIManager.cs
@@ -7,11 +7,20 @@ using DG.Tweening;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] RectTransform Buttons;
+    [SerializeField] RectTransform Config;
     [SerializeField] CanvasGroup StartText;
-    [SerializeField] Button screenButton;
+    [SerializeField] Button ScreenButton;
+
+    [SerializeField] RectTransform CreditsInfo;
+    [SerializeField] CanvasGroup CreditsPanel;
+
 
     void Start()
     {
+        Config.transform.localScale = Vector2.zero;
+
+        ScreenButton.gameObject.SetActive(false);
+
         DOTween.Init();
         Welcome();
     }
@@ -25,14 +34,14 @@ public class UIManager : MonoBehaviour
     {
         StartText.alpha = 0;
         StartText.gameObject.SetActive(true);
-        screenButton.gameObject.SetActive(true);
+        ScreenButton.gameObject.SetActive(true);
 
         StartText.DOFade(1, 0.6f)
             .SetEase(Ease.InQuart)
             .SetLoops(-1, LoopType.Yoyo)
             .SetUpdate(true);
 
-        screenButton.onClick.AddListener(CloseWelcome);
+        ScreenButton.onClick.AddListener(CloseWelcome);
     }
 
     private void CloseWelcome()
@@ -43,6 +52,66 @@ public class UIManager : MonoBehaviour
 
         Buttons.DOAnchorPos(new Vector2(0, 0), 1);
 
-        screenButton.onClick.RemoveListener(CloseWelcome);
+        ScreenButton.onClick.RemoveListener(CloseWelcome);
+        ScreenButton.gameObject.SetActive(false);
     }
+
+    //Config Button
+    public void ConfigUIButton()
+    {
+        Config.gameObject.SetActive(true);
+        Config.DOScale(Vector3.one, 0.8f).SetEase(Ease.OutExpo);
+
+    }
+    public void BackFromConfigUIButton()
+    {
+        Config.DOScale(Vector3.zero, 0.5f).SetEase(Ease.InBack).OnComplete(() => Config.gameObject.SetActive(false));
+    }
+
+
+    #region "Creditos"
+
+    public void OpenCredits()
+    {
+        CreditsInfo.gameObject.GetComponent<CanvasGroup>().DOFade(1, 0);
+        ScreenButton.gameObject.SetActive(true);
+        CreditsInfo.gameObject.SetActive(true);
+
+        CreditsPanel.gameObject.SetActive(true);
+        CreditsPanel.DOFade(1, 1).OnComplete(() => {
+            ScreenButton.onClick.AddListener(CloseCredits);
+            CreditsInfo.DOAnchorPosY(1511, 35).OnComplete(EndCredits);
+        });
+    }
+
+    public void CloseCredits()
+    {
+        CreditsInfo.DOKill();
+        CreditsPanel.DOFade(0, 1).SetEase(Ease.InOutBack).OnComplete(() => CreditsPanel.gameObject.SetActive(false));
+        CreditsInfo.gameObject.GetComponent<CanvasGroup>().DOFade(0, 1).SetEase(Ease.InOutBack).OnComplete(() => {
+            CreditsInfo.gameObject.SetActive(false);
+            CreditsInfo.DOAnchorPosY(-2200, 0);
+        });
+
+        ScreenButton.gameObject.SetActive(false);
+        ScreenButton.onClick.RemoveAllListeners();
+    }
+
+    public void EndCredits()
+    {
+        CreditsInfo.gameObject.GetComponent<CanvasGroup>().DOFade(0, 1).SetDelay(3).SetEase(Ease.InOutBack).OnComplete(() => {
+            CreditsInfo.gameObject.SetActive(false);
+        });
+        CreditsPanel.DOFade(0, 1).SetDelay(3).SetEase(Ease.InOutBack).OnComplete(() =>
+        {
+
+            CreditsPanel.gameObject.SetActive(false);
+            ScreenButton.gameObject.SetActive(false);
+            ScreenButton.onClick.RemoveAllListeners();
+            CreditsInfo.DOAnchorPosY(-2200, 0);
+        });
+    }
+
+    #endregion
+
 }
